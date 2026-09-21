5284b4ef449997e204762ee161146632f547878f
KumarLokeshX <er.lokeshkumardash07@gmail.com>
2026-09-22 00:09:28 +0530

    Initial Commit: Java fundamentals practice

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Main {j
 
     public static void main(String[] args) {
      // Write your code here
